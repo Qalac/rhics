636b336ee722b2f09e184df48af1cd8bd7a7bf78
Qalac <officialqalac@gmail.com>
2021-04-07 01:06:48 +0100

    finished version

--- helper.py
+++ helper.py
@@ -1,7 +1,15 @@
+import requests
+from datetime import date, timedelta
+
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=OFJRQ0NCDVEJLCCB "
+
+time = str(date.today() - timedelta(days=1))
+
 def make_call():
-    return [{"name": "anthony", "age": 11}, {"name": "clara", "age": 90}]
+    response = requests.get(url)
+    response = response.json()
+    return response['Time Series (Daily)'][time]
 
 def resolve():
     results = make_call()
     return results
-
